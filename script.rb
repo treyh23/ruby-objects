9d2b58995537d1684bc9ffb6669b51f7754491b6
treyh23 <forrest@contempows.com>
2018-04-02 19:51:30 -0700

    added private and public methods

--- script.rb
+++ script.rb
@@ -1,11 +1,16 @@
 class Dog
-  public
-  def bark
-    puts "woof!"
-  end
   def initialize(name, breed)
     @name = name
     @breed = breed
   end
+  
+  public
+  def bark
+    puts "woof!"
+  end
+  
+  private
+  def id
+    @id_number = 12345
+  end
 end
-    
